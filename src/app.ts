c8a509e51b568038a1743c8113ce153c82fb7a83
Jalil Kettani <3053619-jkgitlabwork@users.noreply.gitlab.com>
2021-06-10 11:05:04 +0200

    chore(swagger): add support

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,6 @@
+import "reflect-metadata";
 import express from "express";
+import swaggerUi from "swagger-ui-express";
 
 const app = express();
 
@@ -6,5 +8,17 @@ app.use(express.json());
 app.use(express.text());
 
 app.set("port", process.env.PORT || 3000);
+
+app.use(express.static("public"));
+
+app.use(
+  "/docs",
+  swaggerUi.serve,
+  swaggerUi.setup(undefined, {
+    swaggerOptions: {
+      url: "/swagger.json",
+    },
+  })
+);
 
 export default app;
