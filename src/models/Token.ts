75f36de621dddfd945798ce50f66c8ecfa99ec86
Jalil Kettani <3053619-jkgitlabwork@users.noreply.gitlab.com>
2021-06-10 13:15:24 +0200

    feat(token): create model

---
+++ src/models/Token.ts
@@ -0,0 +1,21 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  OneToOne,
+  JoinColumn,
+} from "typeorm";
+import { User } from "./User";
+
+@Entity()
+export class Token {
+  @PrimaryGeneratedColumn()
+  id!: number;
+
+  @Column("int")
+  rate_limit!: number;
+
+  @OneToOne(() => User)
+  @JoinColumn()
+  user_id!: User;
+}
--- src/models/index.ts
+++ src/models/index.ts
@@ -1,3 +1,4 @@
 import { User } from "./User";
+import { Token } from "./Token";
 
-export { User };
+export { User, Token };
