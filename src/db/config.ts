ac27ddb30353cc05781e4b6415aad20ce580de00
Jalil Kettani <3053619-jkgitlabwork@users.noreply.gitlab.com>
2021-06-11 00:49:31 +0200

    fix(configDB): connection depends on DATABASE_URL

--- src/db/config.ts
+++ src/db/config.ts
@@ -1,14 +1,27 @@
 import { ConnectionOptions } from "typeorm";
 import { User, Token, ApiRequest } from "../models";
 
-export const configDB: ConnectionOptions = {
-  type: "postgres",
-  host: process.env.POSTGRES_HOST || "localhost",
-  port: Number(process.env.POSTGRES_PORT) || 5432,
-  username: process.env.POSTGRES_USER || "postgres",
-  password: process.env.POSTGRES_PASSWORD || "jk",
-  database: process.env.POSTGRES_DB || "justify",
-  entities: [User, Token, ApiRequest],
-  synchronize: true,
-  extra: { ssl: true, rejectUnauthorized: false },
+const getOptions = (): ConnectionOptions => {
+  const connectionOptions: ConnectionOptions = {
+    type: "postgres",
+    entities: [User, Token, ApiRequest],
+    synchronize: true,
+  };
+  if (process.env.DATABASE_URL) {
+    Object.assign(connectionOptions, {
+      url: process.env.DATABASE_URL,
+      extra: { ssl: true, rejectUnauthorized: false },
+    });
+  } else {
+    Object.assign(connectionOptions, {
+      host: process.env.POSTGRES_HOST || "localhost",
+      port: Number(process.env.POSTGRES_PORT) || 5432,
+      username: process.env.POSTGRES_USER || "postgres",
+      password: process.env.POSTGRES_PASSWORD || "jk",
+      database: process.env.POSTGRES_DB || "justify",
+    });
+  }
+  return connectionOptions;
 };
+
+export const configDB = getOptions();
