ffe5636f6ec8de7e5bed831a81123cbcd25da67c
Jalil Kettani <3053619-jkgitlabwork@users.noreply.gitlab.com>
2021-06-11 20:49:44 +0200

    test(api): add test for api/justify and api/token routes

--- test/api.test.ts
+++ test/api.test.ts
@@ -9,11 +9,14 @@ import { configDB } from "../src/db/config";
  */
 beforeAll(async () => {
   await createConnection(configDB);
-  await getRepository(User).createQueryBuilder().delete().execute();
-  await getRepository(Token).createQueryBuilder().delete().execute();
   await getRepository(ApiRequest).createQueryBuilder().delete().execute();
+  await getRepository(Token).createQueryBuilder().delete().execute();
+  await getRepository(User).createQueryBuilder().delete().execute();
 });
 
+/**
+ * Test POST on /api/users
+ */
 describe("POST /api/users", () => {
   describe("Correct params", () => {
     it("should return 200 ok", async () => {
@@ -79,6 +82,124 @@ describe("POST /api/users", () => {
         .send({ email: "foo2@bar.com", password: "" });
       expect(result.status).toEqual(400);
       expect(result.text).toBe("Password missing.");
+    });
+  });
+});
+
+/**
+ * Test POST on /api/token
+ */
+describe("POST /api/token", () => {
+  describe("Correct params", () => {
+    it("should return 200 ok", async () => {
+      const result = await request(app)
+        .post("/api/token")
+        .send({ email: "foo@bar.com", password: "password" });
+      expect(result.status).toEqual(200);
+      expect(result.body).toEqual({
+        token: expect.stringMatching(
+          /[a-zA-Z0-9]+\.[a-zA-Z0-9]+\.[a-zA-Z0-9]+/
+        ),
+      });
+    });
+  });
+
+  it("should return 400 (no body)", async () => {
+    const result = await request(app).post("/api/token");
+    expect(result.status).toEqual(400);
+    expect(result.text).toBe("Invalid email or password.");
+  });
+
+  it("should return 400 (missing password)", async () => {
+    const result = await request(app)
+      .post("/api/token")
+      .send({ email: "foo@bar.com" });
+    expect(result.status).toEqual(400);
+    expect(result.text).toBe("Invalid email or password.");
+  });
+
+  it("should return 400 (missing email)", async () => {
+    const result = await request(app)
+      .post("/api/token")
+      .send({ password: "password" });
+    expect(result.status).toEqual(400);
+    expect(result.text).toBe("Invalid email or password.");
+  });
+
+  it("should return 400 (unknown email)", async () => {
+    const result = await request(app)
+      .post("/api/token")
+      .send({ email: "not.registered@bar.com", password: "password" });
+    expect(result.status).toEqual(400);
+    expect(result.text).toBe("Invalid email or password.");
+  });
+
+  describe("Incorrect params", () => {
+    it("should return 400 (wrong password)", async () => {
+      const result = await request(app)
+        .post("/api/token")
+        .send({ email: "foo@bar.com", password: "wrong password" });
+      expect(result.status).toEqual(400);
+      expect(result.text).toBe("Invalid email or password.");
+    });
+  });
+});
+
+/**
+ * Test POST on /api/justify
+ */
+describe("POST /api/justify", () => {
+  describe("Correct params", () => {
+    it("should return 200 ok", async () => {
+      const token = await request(app)
+        .post("/api/token")
+        .send({ email: "foo@bar.com", password: "password" });
+      console.log(token.body.token);
+
+      const result = await request(app)
+        .post("/api/justify")
+        .set("Authorization", `Bearer ${token.body.token}`)
+        .set("Content-Type", "text/plain")
+        .send("This is some text");
+
+      expect(result.text).toBe("This is some text");
+      expect(result.status).toEqual(200);
+    });
+  });
+
+  const TOKEN_EXAMPLE =
+    "fyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImZvb0BiYXIuY29tIiwicGFzc3dvcmQiOiJwYXNzd29yZCIsImlhdCI6MTYyMzQzNTU0MX0.XrDEqG-KCt-H72xHMpTkNarMvdaYR5nvgTU2OiMcICU";
+
+  describe("Incorrect params", () => {
+    /**
+     * Make sure --runInBand option is activated.
+     */
+    it("should return 400 (missing content-type)", async () => {
+      const result = await request(app)
+        .post("/api/justify")
+        .set("Authorization", `Bearer ${TOKEN_EXAMPLE}`)
+        .send("This is some text.");
+      expect(result.status).toEqual(400);
+      expect(result.text).toBe("Expected MIME type: plain/text.");
+    });
+
+    it("should return 403 (missing token)", async () => {
+      const result = await request(app)
+        .post("/api/justify")
+        .set("Content-Type", "text/plain")
+        .send("This is some text.");
+      expect(result.status).toEqual(403);
+      expect(result.text).toBe("Token missing.");
+    });
+
+    it("should return 400 (invalid token)", async () => {
+      const result = await request(app)
+        .post("/api/justify")
+        .set("Authorization", `Bearer ${TOKEN_EXAMPLE}`)
+        .set("Content-Type", "text/plain")
+        .send("This is some text.");
+      expect(result.status).toEqual(400);
+      expect(result.text).toBe("invalid token");
     });
   });
 });
